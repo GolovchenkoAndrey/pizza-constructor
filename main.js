32e4dd8455e49b712cb2cf24dd95fb64588fe7df
Roman (work) <whiten.ncs@gmail.com>
2017-01-17 13:32:26 +0200

    create js on change type pizza

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 //что то должно быть
 $(document).ready(function () {
 
-    //оставляем только средний размер у ход-догов
+    //оставляем только средний размер у ход-догов + выделяем нажатие
     $('.item-oplabel').on('click', function () {
         switch ($(this).attr('for')) {
+            //1 - americano / 2 - italiano
             case 't1':
             case 't2':
-                $('.label-large').css({'display': 'inline-block'});
-                $('.label-small').css({'display': 'inline-block'});
-                $('.label-mid').css({'background-position': ''});
+                $('.item-oplabel').css({'opacity': '.5'});
+                $(this).css({'opacity': '1'});
+                $('.item-grlabel').css({
+                    'display': 'inline-block'
+                });
                 break;
+            //3 - hot-dog
             case 't3':
-                $('.label-large').css({'display': 'none'});
-                $('.label-small').css({'display': 'none'});
-                $('.label-mid').css({'background-position': '50% -56px'});
+                $('.item-grlabel').css({'display': 'none'});
+                $(this).css({'opacity': '1'});
+                $('.label-mid').css({
+                    'display': 'inline-block',
+                    'background-position': '50% -56px'
+                });
                 break;
         }
-
     });
+
+    //ставим окраску у выбра размера
+
 
 });
